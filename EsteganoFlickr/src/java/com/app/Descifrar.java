ba6857082a64f2feec80153ffee2a911c2a2618c
Ruben <Ruben@Ruben-PC>
2015-05-17 17:31:33 +0200

    Descifrar ( subirImg ) completado

--- EsteganoFlickr/src/java/com/app/Descifrar.java
+++ EsteganoFlickr/src/java/com/app/Descifrar.java
@@ -1,24 +1,45 @@
+package com.app;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.app;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.output.*;
+
 /**
  *
  * @author Ruben
  */
-@WebServlet(name = "Descifrar", urlPatterns = {"/Descifrar"})
+@WebServlet(urlPatterns = {"/Descifrar"})
 public class Descifrar extends HttpServlet {
+    
+   private boolean isMultipart;
+   private String filePath;
+   private int maxFileSize = 5000 * 10024;
+   private int maxMemSize = 400 * 10024;
+   private File file ;
+
+   public void init( ){
+      // Get the file location where it would be stored.
+      filePath = getServletContext().getInitParameter("file-upload"); 
+   }
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -34,20 +55,22 @@ public class Descifrar extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>\n" +
-"<html lang=\"es\">\n" +
+            out.println("<html>\n" +
 "<head>\n" +
-"<title>Descifrar</title>\n" +
-"<meta charset=\"utf-8\">\n" +
+"<title>Descifrar Mensaje</title>\n" +
+"</head>\n" +
 "<body>\n" +
-"<form action=\"FileServlet?action=upload\" enctype=\"multipart/form-data\" method=\"post\">\n" +
-"    Fichero: <input type=\"file\" name=\"archivo\"/>\n" +
-"    <input type=\"submit\" value=\"Subir\"/> \n" +
+"<h3>Descifrar Mensaje</h3>\n" +
+"<hr>\n" +
+"Sube la imagen (PNG) :\n" +
+"<p></p>\n" +
+"<form action=\"Descifrar\" method=\"post\" enctype=\"multipart/form-data\">\n" +
+"<input type=\"file\" name=\"file\" size=\"50\" />\n" +
+"<input type=\"submit\" value=\"Subir y Descifrar\" />\n" +
 "</form>\n" +
 "</body>\n" +
-"</head>\n" +
 "</html>");
-           
+
         }
     }
 
@@ -77,9 +100,79 @@ public class Descifrar extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
-    }
+        
+      // Check that we have a file upload request
+      isMultipart = ServletFileUpload.isMultipartContent(request);
+      response.setContentType("text/html");
+      java.io.PrintWriter out = response.getWriter( );
+      if( !isMultipart ){
+         out.println("<html>");
+         out.println("<head>");
+         out.println("<title>Descifrando..</title>");  
+         out.println("</head>");
+         out.println("<body>");
+         out.println("<p>No se ha podido subir</p>"); 
+         out.println("</body>");
+         out.println("</html>");
+         return;
+      }
+      DiskFileItemFactory factory = new DiskFileItemFactory();
+      // maximum size that will be stored in memory
+      factory.setSizeThreshold(maxMemSize);
+      // Location to save data that is larger than maxMemSize.
+      factory.setRepository(new File("c:\\temp"));
 
+      // Create a new file upload handler
+      ServletFileUpload upload = new ServletFileUpload(factory);
+      // maximum file size to be uploaded.
+      upload.setSizeMax( maxFileSize );
+
+      try{ 
+            // Parse the request to get file items.
+            List fileItems = upload.parseRequest(request);
+
+            // Process the uploaded file items
+            Iterator i = fileItems.iterator();
+
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Descifrando..</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            while ( i.hasNext () ) 
+            {
+               FileItem fi = (FileItem)i.next();
+               if ( !fi.isFormField () )	
+               {
+                  // Get the uploaded file parameters
+                  String fieldName = fi.getFieldName();
+                  String fileName = fi.getName();
+                  String contentType = fi.getContentType();
+                  boolean isInMemory = fi.isInMemory();
+                  long sizeInBytes = fi.getSize();
+                  // Write the file
+                  if( fileName.lastIndexOf("\\") >= 0 ){
+                     file = new File( filePath + 
+                     fileName.substring( fileName.lastIndexOf("\\"))) ;
+                  }else{
+                     file = new File( filePath + 
+                     fileName.substring(fileName.lastIndexOf("\\")+1)) ;
+                  }
+                  fi.write( file ) ;
+                  out.println("<h2>");
+                  out.println("Mensaje : ");
+                  out.println("</h2>");
+                  //cambiar el fielName por el mensaje descifrado
+                  out.println("El mensaje es : " + fileName + "<br>");
+               }
+            }
+            out.println("</body>");
+            out.println("</html>");
+     }catch(Exception ex) {
+         System.out.println(ex);
+     }
+   
+  }
     /**
      * Returns a short description of the servlet.
      *
